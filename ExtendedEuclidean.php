d258a7aff7de4045bfe2553ee8785a4eef4087cc
m.ryngier <michal.ryngier@gmail.com>
2021-03-03 20:40:01 +0100

    FEAT: init

---
+++ ExtendedEuclidean.php
@@ -0,0 +1,147 @@
+<?php
+
+include "PrintHelper.php";
+
+class ExtendedEuclidean
+{
+    protected $a;
+    protected $b;
+    protected $results = [];
+    protected $NWD = 1;
+    protected $xValue;
+    protected $yValue;
+
+    public function __construct(int $a, int $b)
+    {
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function main() : void
+    {
+        if ($this->b > $this->a) {
+            $a = $this->b;
+            $b = $this->a;
+        } else {
+            $a = $this->a;
+            $b = $this->b;
+        }
+        PrintHelper::printLine("Działanie rozszerzonego algorytmu Euklidesa dla a = $this->a oraz b = $this->b ");
+        PrintHelper::printLine();
+        do {
+            $r = $a % $b;
+            $integerDiv = (int)floor($a / $b);
+            $multiplicative = (int)($a - $r) / $integerDiv;
+            if ($r !== 0) {
+                PrintHelper::printDouble(
+                    "$a = $integerDiv * $multiplicative + $r",
+                    "$a - $integerDiv * $multiplicative = $r"
+                );
+                $this->results[] = [
+                    "a" => $a,
+                    "integerDiv" => -$integerDiv,
+                    "multiplicative" => $multiplicative,
+                    "r" => $r
+                ];
+            } else {
+                PrintHelper::printLine("$a = $integerDiv * $multiplicative + $r");
+            }
+            $a = $b;
+            $b = $r;
+        } while ($r > 0);
+        if ($this->a % $a === 0 && $this->b % $a === 0) {
+            $this->NWD = $a;
+        }
+        PrintHelper::printLine("NWD($this->a, $this->b) = $this->NWD");
+        PrintHelper::printLine();
+        PrintHelper::printLine($this->getResult());
+        PrintHelper::printLine();
+        PrintHelper::printLine(
+            "NWD($this->a, $this->b) = $this->NWD = $this->a * "
+            . (
+                $this->xValue >= 0 ? $this->xValue : "($this->xValue)"
+            )
+            . " + $this->b * "
+            . (
+                $this->yValue >= 0 ? $this->yValue : "($this->yValue)"
+            )
+        );
+        PrintHelper::printLine();
+        PrintHelper::printLine("x = $this->xValue    y = $this->yValue");
+    }
+
+    protected function getResult() : string
+    {
+        $resultsLastIndex = count($this->results) - 1;
+        $responseString = "$this->NWD = ";
+        for ($i = $resultsLastIndex; $i >= 0; $i--) {
+            $result = $this->results[$i];
+            if ($i === $resultsLastIndex) {
+                $responseString .= $result["a"] . " - " . abs($result["integerDiv"]) . " * " . $result["multiplicative"];
+            } else {
+                if ($i === $resultsLastIndex - 1) {
+                    $previousResult = $this->results[$i + 1];
+                    $responseString .= "\n  = "
+                        . $this->results[$i]["multiplicative"]
+                        . ($previousResult["integerDiv"] >= 0 ? " + " : " - ")
+                        . abs($previousResult["integerDiv"])
+                        . " * ("
+                        . $result["a"] . " - " . abs($result["integerDiv"]) . " * " . $result["multiplicative"]
+                        . ")";
+                    $this->results[$i]["a"] = $result["a"] * $previousResult["integerDiv"];
+                    $this->results[$i]["integerDiv"] = $previousResult["integerDiv"] * $result["integerDiv"] + (
+                        $this->results[$i]["a"] >= 0
+                            ? -1
+                            : 1
+                        );
+                    $responseString .= "\n  = "
+                        . (
+                        $previousResult["integerDiv"] >= 0
+                            ? $previousResult["integerDiv"] . " * "
+                            : "(" . $previousResult["integerDiv"] . ") * "
+                        )
+                        . $result["a"]
+                        . ($this->results[$i]["integerDiv"] >= 0 ? " + " : " - ")
+                        . abs($this->results[$i]["integerDiv"])
+                        . " * "
+                        . $result["multiplicative"];
+                } else {
+                    $previousResult = $this->results[$i + 1];
+                    $responseString .= "\n  = "
+                        . (
+                        $this->results[$i + 2]["integerDiv"] >= 0
+                            ? $this->results[$i + 2]["integerDiv"] . " * "
+                            : "(" . $this->results[$i + 2]["integerDiv"] . ") * "
+                        )
+                        . $this->results[$i]["multiplicative"]
+                        . ($previousResult["integerDiv"] >= 0 ? " + " : " - ")
+                        . abs($previousResult["integerDiv"])
+                        . " * ("
+                        . $result["a"] . " - " . abs($result["integerDiv"]) . " * " . $result["multiplicative"]
+                        . ")";
+                    $this->results[$i]["a"] = $result["a"] * $previousResult["integerDiv"];
+                    $this->results[$i]["integerDiv"] = $result["integerDiv"]
+                        * $previousResult["integerDiv"]
+                        + $this->results[$i + 2]["integerDiv"];
+                    $responseString .= "\n  = "
+                        . $previousResult["integerDiv"]
+                        . " * "
+                        . $result["a"]
+                        . ($this->results[$i]["integerDiv"] >= 0 ? " + " : " - ")
+                        . abs($this->results[$i]["integerDiv"])
+                        . " * "
+                        . $result["multiplicative"];
+                    $this->yValue = $this->results[$i]["integerDiv"];
+                    $this->xValue = $previousResult["integerDiv"];
+                }
+            }
+        }
+        return $responseString;
+    }
+}
+
+$a = 123123;
+$b = 73;
+
+$ee = new ExtendedEuclidean($a, $b);
+$ee->main();
---
+++ PrintHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+class PrintHelper
+{
+    const LINE_WIDTH = 60;
+
+    public static function printLine(string $s = "") : void
+    {
+        echo $s . PHP_EOL;
+    }
+
+    protected static function printSingle($s = "") : void
+    {
+        echo $s;
+    }
+
+    public static function printDouble(string $s1, string $s2) : void
+    {
+        echo self::createDoubleLine($s1, $s2) . PHP_EOL;
+    }
+
+    public static function createDoubleLine(string $s1, string$s2) : string
+    {
+        $firstStringPrinted = false;
+        $responseString = $s1;
+        $spaceLength = self::LINE_WIDTH / 2 - strlen($s1);
+        for ($i = 0; $i < $spaceLength; $i++) {
+            $responseString .= " ";
+            if ($i === $spaceLength - 1 && $firstStringPrinted === false) {
+                $firstStringPrinted = true;
+                $responseString .= "|";
+                $i = 0;
+                $spaceLength = self::LINE_WIDTH / 2 - strlen($s2);
+            }
+        }
+        $responseString .= $s2;
+        return $responseString;
+    }
+}
